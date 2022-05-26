58b67832f47cfc868dd1882e6ed795563b15549f
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-05-26 08:36:27 -0400

    Trying out the blog post https://testdriven.io/blog/modern-tdd/

---
+++ tests/test_ip_ut.py
@@ -0,0 +1,19 @@
+from unittest import mock
+import requests
+from requests import Response
+
+def get_my_ip():
+    response = requests.get('http://ipinfo.io/json')
+    return response.json()['ip']
+
+def test_get_my_ip(monkeypatch):
+    my_ip = '123.123.123.123'
+    response = mock.create_autospec(Response)
+    response.json.return_value = {'ip': my_ip}
+    monkeypatch.setattr(
+        requests,
+        'get',
+        lambda *args, **kwargs: response
+    )
+
+    assert get_my_ip() == my_ip
