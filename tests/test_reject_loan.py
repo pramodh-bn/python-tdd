0d82518fa112e5300dcc9f617ecee5924279a176
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-05-28 15:13:07 -0400

    Trying out the blog post https://testdriven.io/blog/modern-tdd/

--- tests/test_reject_loan.py
+++ tests/test_reject_loan.py
@@ -3,5 +3,10 @@ from app.reject_loan import reject_loan
 
 def test_rejecT_loan():
     loan = Loan(amount=100_000)
+    assert not reject_loan(loan).rejected()
 
+    loan = Loan(amount=250_001)
+    assert reject_loan(loan).rejected()
+
+    loan = Loan(amount=250_000)
     assert not reject_loan(loan).rejected()
