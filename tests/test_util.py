f2cb8534b23af0c01923fd6482828eb55d32797d
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-05-29 09:08:13 -0400

    Trying out the blog post https://testdriven.io/blog/modern-tdd/

---
+++ tests/test_util.py
@@ -0,0 +1,14 @@
+import pytest
+from app.util import increment
+
+@pytest.mark.parametrize(
+    'number, result',
+    [
+        (-2, -1),
+        (0, 1),
+        (3, 4),
+        (101234, 101235)
+    ]
+)
+def test_increment(number, result):
+    assert increment(number) == result
