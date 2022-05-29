c2b85e22b8a7e14d1abb9be230a39c31f5552ea4
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-05-29 09:14:23 -0400

    Trying out the blog post https://testdriven.io/blog/modern-tdd/

---
+++ tests/test_hypothesis.py
@@ -0,0 +1,9 @@
+from hypothesis import given
+import hypothesis.strategies as st
+
+from app.util import increment
+
+@given(st.integers())
+def test_add_one(num):
+    assert increment(num) == num + 1
+
