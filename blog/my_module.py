ff6e335fa5d1269f6621f0f32625852a9b0b797b
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-06-20 09:28:22 -0400

    complexity check

--- blog/my_module.py
+++ blog/my_module.py
@@ -1,14 +1,14 @@
-from requests import *
+from requests import get
+
 
 def get_error_message(error_type):
-    if error_type == 404:
-        return 'red'
-    if error_type == 403:
-        return 'orange'
-    if error_type == 401:
-        return 'yellow'
-    else:
-        return 'blue'
+    colors = {
+        404: 'red',
+        403: 'orange',
+        401: 'yellow',
+    }
+    return colors[error_type] if error_type in colors else 'blue'
+
 
 def main():
     res = get('http://api.gethub.events')
@@ -17,6 +17,7 @@ def main():
         print(f"{STATUS}")
     else:
         print(get_error_message(STATUS))
+
 
 if __name__ == '__main__':
     main()
