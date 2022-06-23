e283b5a803146c3fbb281c2fc9c847d419b20769
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-06-23 08:37:12 -0400

    type check

---
+++ blog/song.py
@@ -0,0 +1,21 @@
+from datetime import date
+from typing import List
+
+from pydantic import BaseModel
+
+class Song(BaseModel):
+    id: int
+    name: str
+    release: date
+    genres: List[str]
+
+song = Song(
+    id=101,
+    name='Bohemian Rhapsody',
+    release='1975-10-31',
+    genres=[
+        'Hard Rock',
+        'Progressive Rock'
+    ]
+)
+print(song)
