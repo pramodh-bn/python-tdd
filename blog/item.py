b3a7f89ebdd383367bb279ae6513d6ed1ab252d0
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-06-24 09:39:40 -0400

    type check

---
+++ blog/item.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+@app.post("/items/", response_model=Item)
+async def create_item(item:Item):
+    return item
